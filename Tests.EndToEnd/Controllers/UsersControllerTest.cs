f1b7f9e4bff0c0dd90a9372d7980e7e7b5e51603
Nodul <kamil.aniszkiewicz@gmail.com>
2017-04-18 21:53:56 +0200

    Added stuff from Episode XII - mostly tests

---
+++ Tests.EndToEnd/Controllers/UsersControllerTest.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Passenger.Api;
+using Newtonsoft.Json;
+using Passenger.Infrastructure.DTO;
+using System.Threading.Tasks;
+using Passenger.Infrastructure.Commands.Users;
+
+namespace Tests.EndToEnd.Controllers
+{
+    [TestClass]
+    public class UsersControllerTest
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public UsersControllerTest()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                                        .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task CheckIfUserExists_ValidEmail_True()
+        {
+            var email = "user2@gmail.com";
+            var user = await GetUserAsync(email);
+
+            Assert.AreEqual("user2@gmail.com", user.Email);
+            // Assert.AreEqual("Hello world",responseString);          
+        }
+
+        [TestMethod]
+        public async Task CheckIfUserExists_InvalidEmail_404()
+        {
+            var email = "user-500@gmail.com";
+            var response = await _client.GetAsync($"users/{email}");
+            Assert.AreEqual(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+
+        }
+        [TestMethod]
+        public async Task CheckIfUserWasCreatedSuccesfully_ValidEmail_201()
+        {
+            var email = "test201@gmail.com";
+            var request = new CreateUser() // might be anonymous as well, just make sure the fields match
+            {
+                Email = email,
+                Username = "test201",
+                Password = "secret_201"
+
+            };
+            var payload = GetPayload(request);
+            var response = await _client.PostAsync("users",payload);
+            Assert.AreEqual(System.Net.HttpStatusCode.Created, response.StatusCode);
+            Assert.AreEqual($"users/{request.Email}",response.Headers.Location.ToString());
+
+            var user = await GetUserAsync(request.Email);
+            Assert.AreEqual("test201@gmail.com", request.Email);
+
+        }
+
+        private async Task<UserDTO> GetUserAsync(string email)
+        {
+            var response = await _client.GetAsync($"users/{email}");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<UserDTO>(responseString);
+        }
+
+        private static StringContent GetPayload(object data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+
+            // the 3rd arguments lets the server know we want json. If we wanted xml it would be application/xml
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+    }
+}
---
+++ Tests/Infrastructure/Services/UserServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Passenger.Core.Domain;
+using Passenger.Core.Repositories;
+using Passenger.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Infrastructure.Services
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public async Task RegisterAsyncShouldInvokeAddAsyncUserService_Once_True()
+        {
+            var userRepoMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var userService = new UserService(userRepoMock.Object,mapperMock.Object);
+            await userService.RegisterAsync("user0@gmail.com","user0","password");
+
+            // we are veryfing that our userService.RegisterAsync called AddAsync once
+            userRepoMock.Verify(x => x.AddAsync(It.IsAny<User>()),Times.Once);
+        }
+    }
+}
